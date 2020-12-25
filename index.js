7255589bef9a70a7a3f8bbf04868eab103e2042b
heyu-077 <1834617305@qq.com>
2020-12-25 13:17:16 +0800

    静态效果完成

---
+++ index.js
@@ -0,0 +1,12 @@
+const boxBg = ['#f44336', '#e91e63', '#9c27b0', '#673ab7', '#3f51b5', '#2196f3', '#03a9f4', '#00bcd4', '#009688', '#4caf50', '#8bc34a', '#cddc39', '#ffeb3b', '#ffc107', '#ff9800', '#ff5722', '#795548', '#564545', '#607d8b', '#405d6b', '#9e9e9e', '#70737d', '#389fa0', '#38a05e', '#b3c981', '#76a803', '#fecf43', '#e2785f'];	//box背景色
+const bodyBg = ['#F7E8ED', '#F2D9E6', '#ECC6DE', '#E0ECF5', '#DDF4DE', '#F0F1D5', '#EEDECD', '#B8E6B3', '#ABE3D8', '#E0E1F5', '#F7E8ED', '#F2D9E6', '#E0ECF5', '#DDF4DE', '#F0F1D5', '#EEDECD', '#B8E6B3', '#ABE3D8', '#DFD1F0', '#6161616'];	//body背景色
+
+const style=document.createElement('style');
+let styleStr='';
+boxBg.forEach((item,index)=>{
+    styleStr += `.box:nth-child(${index+1})>div{
+        background: ${item} url("./images/${index+1}.png") no-repeat center;
+    }`
+});
+style.innerHTML=styleStr;
+document.head.appendChild(style);
